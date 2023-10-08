6cec4a15ec8f844c64037e48756dc4ad4b1e2272
Rintia Islam <rintiaislam110@gmail.com>
2023-10-08 19:20:50 +0600

    footer added

--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 
 const Root = () => {
     return (
-        <div>
+        <div className="relative bg-green-600 min-h-screen">
             <Header></Header>
             <Outlet></Outlet>
             <Footer></Footer>
